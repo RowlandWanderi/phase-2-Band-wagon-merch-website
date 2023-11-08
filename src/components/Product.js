888bcb4fa0c2c43e32272f9c7d09ce80b9bba318
Rowland <rowland.wanderi@student.moringaschool.com>
2023-11-08 22:30:47 +0300

    styled my buttons

--- src/components/Product.js
+++ src/components/Product.js
@@ -83,20 +83,29 @@ const checkAdd = insidecart ? "btn btn-danger" : "btn btn-success"
   }
 
   return (
-    <div className='card col-md-3 mb-3 '>
+    <div className=' ui card col-md-3 mb-3 ' key={product.id}>
       <img src={product.image_url} alt="Loading" className="img-fluid"/>
       <div className='card-body'>
       <h5 className="card-title">{product.name}</h5>
        <p className="card-text">{product.description}</p>
        <p className="card-text">Kshs &nbsp;{product.price}</p>
-       <button className={checkAdd} onClick={() => handleCart()} >{product.is_inCart ? "Remove From Cart" : "Add To Cart"}</button>
-       <Link to={`/products/${product.id}`}>
-          <button type="button" className="btn btn-info m-1">Update Product Details</button>
-       </Link>
-       <Link to={`reviews/${product.id}`}>
-          <button type="button" className="btn btn-info m-1">Review this product</button>
-       </Link>
-       <button onClick={()=> deleteProduct()}type="button" className="btn btn-danger m-1">Delete Item</button>
+       <div className='d-grid gap-2'>
+          <button type="button" className={checkAdd} onClick={() => handleCart()} >{product.is_inCart ? "Remove From Cart" : "Add To Cart"}</button>
+          
+              <button type="button" className="btn btn-info m-1">
+                <Link id='link' to={`/products/${product.id}`}>
+                Update Product Details
+                </Link>
+              </button>
+              <button type="button" className="btn btn-info m-1">
+              <Link id='link'to={`reviews/${product.id}`}>
+                Review this product
+                </Link>
+              </button>
+          
+          <button type="button" onClick={()=> deleteProduct()} className="btn btn-danger m-1">Delete Item</button>
+       </div>
+      
       
       </div>
     </div>
